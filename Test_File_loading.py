13cf7da20f2c4f9c96ff5ef6ea62f4c22927355e
Ambrose Pravin <44547275+pravinag1987@users.noreply.github.com>
2021-02-18 21:44:23 +0530

    Adding Configuration file
    Now all the parameters  are passing through Configuration file.

--- Test_File_loading.py
+++ Test_File_loading.py
@@ -1,8 +1,14 @@
 import os
+import configparser
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import current_date
 from pyspark.sql.types import *
 from datetime import datetime
+
+config = configparser.ConfigParser()
+config.read('proj_config')
+
+## Need to enable this block of code
 
 uschema = StructType([\
     StructField("DataExtractDate",StringType(),True), \
@@ -15,7 +21,8 @@ uschema = StructType([\
 
 spark =  SparkSession.builder.master("local").appName("Test_File_loading").enableHiveSupport().getOrCreate()
 ## Send as parameter
-file_path = '/home/hduser1/PravinFiles/Sparkfiles/data/AirLineData/Test_Input_data/'
+##file_path = '/home/hduser1/PravinFiles/Sparkfiles/data/AirLineData/Test_Input_data/'
+file_path = config.get('project_settings', 'file_pathh')
 
 for file in os.listdir(file_path):
    file_name, file_extension = os.path.splitext(file)
@@ -26,10 +33,30 @@ for file in os.listdir(file_path):
       file_read = spark.read.option("header", "true").schema(uschema).csv(filess)
       file_read1 = file_read.withColumn("load_date", current_date())
       ## Hive table name have send as parameter
-      get_hive_dt = spark.sql("select max(load_date) from test_spark.tbl_airline_data_orc").first()[0]
+      ##get_hive_dt = spark.sql("select max(load_date) from test_spark.tbl_airline_data_orc").first()[0]
+      get_hive_dt = spark.sql("select max(load_date) from "+ config.get('project_settings', 'raw_table') +"" ).first()[0]
 
       current_dt = datetime.now().strftime("%Y-%m-%d")
 
       if (str(get_hive_dt) == 'None') or (str(current_dt) == str(get_hive_dt)):
 
-         file_read1.write.format("orc").mode("append").insertInto("test_spark.tbl_airline_data_orc")
+         file_read1.write.format("orc").mode("append").insertInto(config.get('project_settings', 'raw_table'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##from pyspark import SparkConf, SparkContext
+##sc = SparkContext(master="local",appName="Spark Demo")
+##print(sc.textFile('file:///home/hduser1/PravinFiles/Sparkfiles/data/AirLineData/Test_Input_data/Los_Angeles_International_Airport_-_Passenger_Count_By_Carrier_Type.csv').first())
--- Test_load_partition_tab.py
+++ Test_load_partition_tab.py
@@ -1,26 +1,30 @@
 import os
+import configparser
 from pyspark.sql import SparkSession
 from datetime import datetime
 from subprocess import call
 
+config = configparser.ConfigParser()
+config.read('proj_config')
+
 ## Send as parameter
-file_path = '/home/hduser1/PravinFiles/Sparkfiles/data/AirLineData/Test_Input_data/'
+file_path = config.get('project_settings', 'file_pathh')
 ## Send as parameter
-scriptt = 'tbl_airline_data_orc.ddl'
+scriptt = config.get('project_settings', 'ddl_script')
 spark =  SparkSession.builder.master("local").appName("Test_Load_partition_tbl").enableHiveSupport().getOrCreate()
 ## Send as parameter
-raw_dataa = spark.sql("select * from test_spark.tbl_airline_data_orc")
+raw_dataa = spark.sql(config.get('project_settings', 'raw_tab_query'))
 
 ## Hive table name have send as parameter
-get_hive_dt = spark.sql("select max(load_date) from test_spark.tbl_airline_data_orc").first()[0]
+get_hive_dt = spark.sql("select max(load_date) from "+ config.get('project_settings', 'raw_table') +"").first()[0]
 
 current_dt = datetime.now().strftime("%Y-%m-%d")
 
 if str(current_dt) == str(get_hive_dt):
   ## Send as parameter
-  raw_dataa.write.format("orc").mode("overwrite").insertInto("test_spark.tbl_airline_data_orc_par")
+  raw_dataa.write.format("orc").mode("overwrite").insertInto(config.get('project_settings', 'partition_table'))
 else:
   ## Send as parameter (After insert into final partition table we need to drop raw table
-  spark.sql("drop table test_spark.tbl_airline_data_orc")
-  ## call unix shell to run hive command (Again creat empty raw table)
+  spark.sql("drop table "+ config.get('project_settings', 'raw_table') +"")
+  ## Send as parameter (Again creat empty raw table)
   call('hive -f "'+ file_path + scriptt +'"', shell=True)
