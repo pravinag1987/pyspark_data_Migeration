ad328ee20b21e1e88caed51c7eeff3c962105397
Ambrose Pravin <44547275+pravinag1987@users.noreply.github.com>
2021-04-13 23:16:28 +0530

    Add files via upload
    Error fixed code

---
+++ Test_spark_save_error_info.py
@@ -0,0 +1,36 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.window import Window
+from pyspark.sql.types import *
+
+
+
+
+
+spark =  SparkSession.builder.master("local").appName("Test_File_loading").enableHiveSupport().getOrCreate()
+schema = StructType([])
+##df = spark.createDataFrame(spark.sparkContext.emptyRDD(), schema)
+
+randomList = ['a', 0, 2]
+
+for entry in randomList:
+    try:
+
+        r = 1/int(entry)
+        break
+    except Exception as e:
+
+        err_data = repr(e)
+
+        df = spark.createDataFrame([err_data], StringType())
+
+        df.write.mode("append").insertInto("test_spark.test_log")
+
+
+
+
+
+
+
+
+
